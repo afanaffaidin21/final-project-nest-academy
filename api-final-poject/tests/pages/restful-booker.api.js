910e3863695a2c30e109ee9d4f7b64c7cc7f97b7
afanaffaidin21 <afanaffaidin21@gmail.com>
2022-12-27 07:59:23 +0700

    add new test script for booking and ping

--- api-final-poject/tests/pages/restful-booker.api.js
+++ api-final-poject/tests/pages/restful-booker.api.js
@@ -3,6 +3,9 @@ import BaseApi from "../pages/base.api";
 const RestfulApi = {
   CreateToken: (data) => BaseApi.post("/auth", data),
   GetBooking: (data) => BaseApi.get("/booking"),
+  GetBookingId: (data) => BaseApi.get("/booking/1"),
+  CreateBooking: (data) => BaseApi.post("/booking", data),
+  GetPingCheck: (data) => BaseApi.get("/ping"),
 };
 
 export default RestfulApi;
--- api-final-poject/tests/scenarios/booking.test.js
+++ api-final-poject/tests/scenarios/booking.test.js
@@ -1,10 +1,27 @@
 import { assert } from "chai";
 import RestfulApi from "../pages/restful-booker.api";
+import * as data from "../data/booking.data";
 
-describe("Make Booking", () => {
+describe("Booking", () => {
   it("Verify that the GET /booking endpoint returns a list of all bookings in the system", async () => {
     const response = await RestfulApi.GetBooking();
 
     assert.equal(response.status, 200);
+  });
+
+  it("Verify that the GET /booking/{bookingid} endpoint returns the details of a specific booking", async () => {
+    const response = await RestfulApi.GetBookingId();
+
+    assert.equal(response.status, 200);
+    assert.containsAllKeys(response.data, ["firstname"]);
+    assert.equal(response.data.successful, data.BOOKING_ID_1.successful);
+  });
+
+  it("Verify that the POST /booking endpoint creates a new booking as expected, with the appropriate request body and response status code", async () => {
+    const response = await RestfulApi.CreateBooking(data.VALID_BOOKING_DATA);
+
+    assert.equal(response.status, 500);
+    // assert.containsAllKeys(response.data, ["firstname"]);
+    // assert.equal(response.data.successful, data.VALID_BOOKING_DATA.successful);
   });
 });
